11ce744386f3aa43d198c1d1630e6ca7dfde7884
Egil Bukhave Edström <ege001@edu.zealand.dk>
2025-11-25 10:03:18 +0100

    storedfileinto class added

---
+++ FileHostingBackend/Models/StoredFileInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileHostingBackend.Models
+{
+    public class StoredFileInfo
+    {
+        public string ID { get; set; }
+        public Folder ParentFolder { get; set; }
+        public string Name { get; set; }
+        public int Size { get; set; } // Size in bytes
+        public DateTime UploadedAt { get; set; }
+        public User UploadedBy { get; set; }
+        public DateTime LastModifiedAt { get; set; }
+        public string FilePath { get; set; } // Path in the storage system
+        public string ShareLink { get; set; } // Public shareable link
+        public StoredFileInfo() { }
+    }
+}
