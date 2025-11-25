1dffebb2f14c3f5bf6f28d1c26d40d11df053e33
Egil Bukhave Edström <ege001@edu.zealand.dk>
2025-11-25 11:01:01 +0100

    1

--- FileHostingBackend/Models/StoredFileInfo.cs
+++ FileHostingBackend/Models/StoredFileInfo.cs
@@ -9,7 +9,7 @@ namespace FileHostingBackend.Models
     public class StoredFileInfo
     {
         public string ID { get; set; }
-        public Folder Folder { get; set; }
+        public Folder? Folder { get; set; }
         public string Name { get; set; }
         public int Size { get; set; } // Size in bytes
         public DateTime UploadedAt { get; set; }
@@ -17,6 +17,7 @@ namespace FileHostingBackend.Models
         public DateTime LastModifiedAt { get; set; }
         public string FilePath { get; set; } // Path in the storage system
         public string ShareLink { get; set; } // Public shareable link
+        public string BucketName { get; set; } // Storage bucket name
         public StoredFileInfo() { }
     }
 }
