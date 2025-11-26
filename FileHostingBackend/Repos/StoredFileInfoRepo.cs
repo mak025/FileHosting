d020423ad7b72db36b3c10698825c3e2a7462ead
Christian Vase Frederiksen <chf004@edu.zealand.dk>
2025-11-26 10:43:24 +0100

    Made StoredFileInfoRepo with methodes

--- FileHostingBackend/Repos/StoredFileInfoRepo.cs
+++ FileHostingBackend/Repos/StoredFileInfoRepo.cs
@@ -1,12 +1,15 @@
-﻿using Minio;
+﻿using FileHosting.Models;
+using FileHostingBackend.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+using Minio;
+using Minio.DataModel.Args;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime;
 using System.Text;
 using System.Threading.Tasks;
-using FileHostingBackend.Models;
-using Microsoft.Extensions.Options;
-using FileHosting.Models;
 
 namespace FileHostingBackend.Repos
 {
@@ -15,12 +18,84 @@ namespace FileHostingBackend.Repos
 
         private readonly IMinioClient _minioClient;
         private readonly string _bucketName;
+        private readonly MinioSettings _settings;
 
         public StoredFileInfoRepo(IOptions<MinioSettings> settings)
         {
+            var config = settings.Value;
+            _bucketName = config.BucketName;
 
+            _minioClient = new MinioClient()
+                                .WithEndpoint(config.Endpoint)
+                                .WithCredentials(config.AccessKey, config.SecretKey)
+                                .Build();
+            EnsureBucketExistsAsync().Wait();
         }
 
+        private async Task EnsureBucketExistsAsync()
+        {
+            bool found = await _minioClient.BucketExistsAsync(new BucketExistsArgs().WithBucket(_settings.BucketName));
+            if (!found)
+            {
+                await _minioClient.MakeBucketAsync(new MakeBucketArgs().WithBucket(_settings.BucketName));
+            }
+        }
+        public async Task<string> UploadFileAsync(IFormFile file)
+        {
+            var fileName = $"{Guid.NewGuid()}_{file.FileName}";
 
+            using var stream = file.OpenReadStream();
+            var putArgs = new PutObjectArgs()
+                .WithBucket(_bucketName)
+                .WithObject(fileName)
+                .WithStreamData(stream)
+                .WithObjectSize(file.Length)
+                .WithContentType(file.ContentType);
+
+            await _minioClient.PutObjectAsync(putArgs);
+            return fileName;
+        }
+
+        public async Task<List<StoredFileInfo>> GetAllFilesAsync()
+        {
+            var files = new List<StoredFileInfo>();
+            var listArgs = new ListObjectsArgs().WithBucket(_bucketName);
+
+            await foreach (var item in _minioClient.ListObjectsEnumAsync(listArgs))
+            {
+                files.Add(new StoredFileInfo
+                {
+                    Name = item.Key,
+                    Size = (int)item.Size,
+                    LastModifiedAt = item.LastModifiedDateTime ?? DateTime.MinValue
+                });
+            }
+
+            return files;
+        }
+
+        public async Task<Stream> DownloadFileAsync(string fileName)
+        {
+            var memoryStream = new MemoryStream();
+            var getArgs = new GetObjectArgs()
+                .WithBucket(_bucketName)
+                .WithObject(fileName)
+                .WithCallbackStream((stream) => stream.CopyTo(memoryStream));
+
+            await _minioClient.GetObjectAsync(getArgs);
+            memoryStream.Position = 0;
+            return memoryStream;
+        }
+        public async Task DeleteFileAsync(string fileName)
+        {
+            var deleteArgs = new RemoveObjectArgs()
+                .WithBucket(_bucketName)
+                .WithObject(fileName);
+            await _minioClient.RemoveObjectAsync(deleteArgs);
+        }
     }
 }
+
+
+    
+
