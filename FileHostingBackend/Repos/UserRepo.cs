7ccf94f86b6e61ba4549ae9ba70c6c69dfaf86b4
Christian Vase Frederiksen <chf004@edu.zealand.dk>
2025-11-26 12:12:18 +0100

    fix

--- FileHostingBackend/Repos/UserRepo.cs
+++ FileHostingBackend/Repos/UserRepo.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using FileHosting.Models;
 using FileHostingBackend.Models;
 using Microsoft.Data.SqlClient;
 
