c9e538d9995c307ad1d50bf5e661048cbc5db3f6
Christian Vase Frederiksen <chf004@edu.zealand.dk>
2025-11-26 12:10:44 +0100

    UserService Created

--- FileHostingBackend/Services/UserService.cs
+++ FileHostingBackend/Services/UserService.cs
@@ -3,10 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FileHostingBackend.Repos;
 
 namespace FileHostingBackend.Services
 {
-    internal class UserService
+    public class UserService
     {
+
+        private readonly IUserRepo _userRepo;
+
+        public UserService(IUserRepo userRepo)
+        {
+            _userRepo = userRepo;
+        }
+        public void CreateUser(string name, string email, string address, string phoneNumber, object? union, int userType)
+        {
+            _userRepo.CreateUser(name, email, address, phoneNumber, union, userType);
+        }
+
+        public void GetUserById(int userId)
+        {
+            _userRepo.GetUserById(userId);
+        }
+
+        public void UpdateUser(int userId, string name, string email, string address, string phoneNumber, object? union, int userType)
+        {
+            _userRepo.UpdateUser(userId, name, email, address, phoneNumber, union, userType);
+        }
+
+        public void DeleteUser(int userId)
+        {
+            _userRepo.DeleteUser(userId);
+        }
+
+
+
     }
 }
