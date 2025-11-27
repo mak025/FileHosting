f0f522e391cdc5dd10f891d09ae5f1c57c220837
Christian Vase Frederiksen <chf004@edu.zealand.dk>
2025-11-27 10:29:14 +0100

    Fileoverview page

---
+++ FileHosting/Pages/FileOverview.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FileHosting.Pages
+{
+    public class FileOverviewModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
