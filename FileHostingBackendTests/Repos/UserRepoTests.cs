b32b1b377710d831f01b6def31c99bf291882ebd
Egil Bukhave Edström <ege001@edu.zealand.dk>
2025-12-17 11:33:09 +0100

    UnitTest

---
+++ FileHostingBackendTests/Repos/UserRepoTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FileHostingBackend.Repos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileHostingBackend.Repos.Tests
+{
+    [TestClass()]
+    public class UserRepoTests
+    {
+        [TestMethod()]
+        public void DeleteUserAsyncTest()
+        {
+            //Arrange
+
+
+            //Act
+
+            //Assert
+            Assert.Fail();
+        }
+    }
+}
